7841c1cafe003abeb4a94e51b49e973825fd8aef
pedro <pedroflopes1710@gmail.com>
2024-02-23 16:42:10 -0300

    ex04

---
+++ lista_01/ex04/ContaCorrente.java
@@ -0,0 +1,37 @@
+package lista_01.ex04;
+
+public class ContaCorrente {
+    private double saldo;
+
+    public ContaCorrente(double saldoAtual) {
+        this.saldo = saldoAtual;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public boolean registrarDeposito(double valor) {
+        if (valor <= 0) {
+            return false;
+        }
+
+        saldo += valor;
+
+        return true;
+    }
+
+    public boolean registrarSaque(double valor) {
+        double tarifa = valor * 0.005;
+        double totalDebito = valor + tarifa;
+    
+        if (valor <= 0 || totalDebito > saldo) {
+            System.out.println("Saque não autorizado: valor inválido ou saldo insuficiente");
+            return false;
+        } 
+    
+        saldo -= totalDebito;
+        System.out.println("Saque realizado com sucesso. Novo saldo: " + saldo);
+        return true;
+    }   
+}
